5a0a7942bd4213f63fe4ac7731465a51c5ce5a90
alex lima <delimaferreiraalex@gmail.com>
2024-04-03 17:36:35 -0300

    feat: prepare server to receive and send messages to the client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,34 @@
 import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
 
 const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.status(200).json({ msg: "hello!" });
+app.use(cors({ origin: "*" }));
+
+const io = new Server(4000, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+    console.log(`connected with id ${socket.id}`);
+    socket.on("message", async (interval: number) => {
+        if (Number.isInteger(interval)) return;
+        await delay(interval * 1000);
+        socket.emit("message", "work is finished!");
+        console.log("message sent");
+    });
 });
+
+async function delay(time: number): Promise<string> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("hello amigo!");
+        }, time);
+    });
+}
+
+app.get("/", async (req, res) => {});
 
 app.listen(port, () => {
     console.log(`app running at port ${port}`);
